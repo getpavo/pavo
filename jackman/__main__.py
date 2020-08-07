9947a2064db1d687d3520494bcce3615615ff4a5
j.veldhuis <jobbfveldhuis@gmail.com>
2020-08-07 02:18:39 +0200

    Made functions in __main__.py part of app class.

--- jackman/__main__.py
+++ jackman/__main__.py
@@ -10,44 +10,59 @@ from errors import *
 from helpers import get_cwd, get_sd, set_dir
 
 
-def create_parser():
-    p = argparse.ArgumentParser(description="Work with Jackman via command line interface",
-                                epilog="For more information, please refer to the documentation.")
-    p.add_argument('command', metavar='CMD', help='The command Jackman should execute.', nargs='+')
-    p.add_argument('--verbose', '-v', help='Whether or not to log actions to the console.', action='store_true')
+class Jackman(object):
+    def __init__(self):
+        self.argument_parser = self.create_parser()
+        self.arguments = vars(self.argument_parser.parse_args())
 
-    return p
+    def execute(self, command=None):
+        try:
+            command = self.arguments['command'][0]
+        except KeyError:
+            pass
 
+        if command == 'create':
+            try:
+                self.new_project(self.arguments['command'][1])
+            except IndexError:
+                self.new_project()
 
-def _new_workspace(name='jackman-project'):
-    if os.path.exists(name) and os.path.isdir(name) and len(os.listdir(name)) != 0:
-        raise FileExistsError('The specified directory already exists and contains files.')
+        elif command == 'build':
+            pass
+        elif command == 'preview':
+            pass
+        elif command == 'deploy':
+            pass
+        else:
+            raise UnknownCliCommandError('Could not execute specified command. It does not exist.')
 
-    try:
-        os.mkdir(name)
-    except FileExistsError:
-        pass
+    @staticmethod
+    def create_parser():
+        p = argparse.ArgumentParser(description="Work with Jackman via command line interface",
+                                    epilog="For more information, please refer to the documentation.")
+        p.add_argument('command', metavar='CMD', help='The command Jackman should execute.', nargs='+')
+        p.add_argument('--verbose', '-v', help='Whether or not to log actions to the console.', action='store_true')
 
-    copy_tree(f'{get_sd()}/templates/empty_project/', f'{get_cwd()}/{name}')
+        return p
+
+    def new_project(self, name='jackman-project'):
+        if os.path.exists(name) and os.path.isdir(name) and len(os.listdir(name)) != 0:
+            raise FileExistsError('The specified directory already exists and contains files.')
+
+        try:
+            os.mkdir(name)
+        except FileExistsError:
+            pass
+
+        copy_tree(f'{get_sd()}/templates/empty_project/', f'{get_cwd()}/{name}')
+        self.__log('', 'success')
+
+    @staticmethod
+    def __log(message, sort='Message'):
+        print(message)
 
 
 if __name__ == '__main__':
     cwd = set_dir(get_cwd())
-    parser = create_parser()
-    args = vars(parser.parse_args())
-    command = args['command'][0]
-
-    if command == 'create':
-        try:
-            _new_workspace(args['command'][1])
-        except IndexError:
-            _new_workspace()
-
-    elif command == 'build':
-        pass
-    elif command == 'preview':
-        pass
-    elif command == 'deploy':
-        pass
-    else:
-        raise UnknownCliCommandError('Could not execute specified command. It does not exist.')
+    app = Jackman()
+    app.execute()
