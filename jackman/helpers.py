193bb999cfd20053235fc8dd2709a86a28d2f333
j.veldhuis <jobbfveldhuis@gmail.com>
2020-08-23 18:20:15 +0200

    Added yaml-loading and docstrings to helpers

--- jackman/helpers.py
+++ jackman/helpers.py
@@ -1,19 +1,49 @@
 import os
 import time
+import yaml
 
 from colorama import Fore, Style
 
 
 # Directory management checks
 def get_cwd():
+    """
+    Retrieves the current working directory.
+
+    Returns
+    -------
+    cwd : str
+        The path to the current working directory.
+    """
     return os.getcwd()
 
 
-def get_sd():
+def get_jackman_dir():
+    """
+    Retrieves the path to the installation folder of the Jackman module.
+
+    Returns
+    -------
+    path : str
+        The path to the jackman module folder.
+    """
     return os.path.dirname(os.path.abspath(__file__))
 
 
 def set_dir(directory):
+    """
+    Changes the current directory to the specified directory.
+
+    Parameters
+    ----------
+    directory : str
+        The path to the directory to change to.
+
+    Returns
+    -------
+    complete : bool
+        Whether or not the directory was changed.
+    """
     try:
         os.chdir(str(directory))
         return True
@@ -22,12 +52,21 @@ def set_dir(directory):
 
 
 def cd_is_project():
+    """
+    Checks whether or not the current directory is a Jackman project.
+
+    Returns
+    -------
+    project : bool
+        Whether or not the current directory can be marked as an initialized jackman project.
+    """
     if os.path.isfile('_jackman_config.yaml') or os.path.isfile('_jackman_config.yml'):
         return True
     return False
 
 
 def log(message, sort='message'):
+    # TODO: Rewrite log method to incorporate and use Python logging module
     sort = sort.lower()
     prefix = {
         'error': Fore.RED,
@@ -52,5 +91,43 @@ def log(message, sort='message'):
     print(f'{prefix}[{current_time}] {message}{suffix}')
 
 
-class Logger:
-    pass
+def load_yaml(file):
+    """
+    Loads a yaml-file into a dictionary.
+
+    Parameters
+    ----------
+    file : str
+        The path to the file that should be loaded.
+
+    Returns
+    -------
+    items : dict
+        A dict with all the items in the yaml-file.
+    """
+    if not is_yaml(file):
+        # TODO: This should raise a warning in the logging module.
+        return {}
+
+    with open(file, 'r') as f:
+        items = yaml.load(f, Loader=yaml.FullLoader)
+
+    return items
+
+def is_yaml(file):
+    """
+    Checks whether or not the specified file is a yaml-file.
+
+    Parameters
+    ----------
+    file : str
+        The relative path to the file that should be checked.
+
+    Returns
+    -------
+    bool
+        Whether or not the specified file is a yaml-file.
+    """
+    if file.endswith('.yaml') or file.endswith('.yml'):
+        return True
+    return False
