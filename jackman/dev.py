a00a6f37975bb814d41007823d4b34f3355fac89
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-18 19:56:05 +0100

    Added logging to dev.py

--- jackman/dev.py
+++ jackman/dev.py
@@ -8,29 +8,38 @@ import http.server
 from jackman.build import Builder
 from jackman.helpers import get_cwd
 
+log = logging.getLogger(__name__)
+
 
 def serve_local_website():
-    logging.getLogger('dev')
+    log.info('Starting a temporary build with mode "development"')
     builder = Builder('development')
     builder.build()
     serve_forever(f'{get_cwd()}/{builder.tmp_dir}')
 
 
 def serve_forever(directory, port=8000):
-    # TODO: Add handler for server stop on KeyboardInterrupt
-    # TODO: Move this to another file
+    log.debug(f'Moving into {directory}')
     os.chdir(directory)
+
     TCPHandler = http.server.SimpleHTTPRequestHandler
     socketserver.TCPServer.allow_reuse_address = True
+    log.debug(f'Created TCP Handler http.server.SimpleHTTPRequestHandler')
 
     with socketserver.TCPServer(("", port), TCPHandler) as dev_server:
         try:
+            log.info(f'Serving forever on localhost:{port}')
             dev_server.serve_forever()
         except KeyboardInterrupt:
+            log.debug('Detected KeyboardInterrupt. Telling server to shutdown.')
             dev_server.shutdown()
             dev_server.server_close()
             dev_server.socket.close()
+            log.info('Succesfully closed the server.')
             shutil.rmtree(directory)
+            log.debug(f'Removed temporary directory {directory}')
         except Exception as e:
-            logging.exception(e)
+            log.exception(e)
             shutil.rmtree(directory)
+            log.debug(f'Removed temporary directory {directory}')
+
