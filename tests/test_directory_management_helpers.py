aae875abbcac427ac00a5b6ef174c74b658124aa
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-20 19:27:41 +0100

    Split tests between multiple files

---
+++ tests/test_directory_management_helpers.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+
+from jackman.helpers import get_cwd, set_dir, cd_is_project
+
+
+def test_get_cwd():
+    assert get_cwd() == os.getcwd()
+
+
+def test_set_dir_not_exists(tmpdir):
+    assert set_dir(f'{tmpdir}/unknown/') is False
+
+
+def test_set_dir_exists(tmpdir):
+    tmpdir.mkdir('existing-folder')
+    assert set_dir(f'{tmpdir}/existing-folder/') is True
+
+
+def test_is_jackman_project_false(tmpdir):
+    set_dir(tmpdir)
+    assert cd_is_project() is False
+
+
+def test_is_jackman_project_true(tmpdir):
+    f = tmpdir.mkdir('project').join('_jackman_config.yaml')
+    f.write('empty')
+    set_dir(f'{tmpdir}/project')
+    assert cd_is_project() is True
