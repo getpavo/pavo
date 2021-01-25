67e02253a1453ef8285557fe92429257076322c9
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-26 00:10:41 +0100

    Added proper Google Docstring support and added getter for configuration values

--- jackman/helpers.py
+++ jackman/helpers.py
@@ -1,8 +1,8 @@
 import logging
 import logging.config
 import os
+from functools import reduce
 
-import htmlmin
 import yaml
 
 log = logging.getLogger(__name__)
@@ -10,42 +10,31 @@ log = logging.getLogger(__name__)
 
 # Directory management checks
 def get_cwd():
-    """
-    Retrieves the current working directory.
+    """Retrieves the current working directory.
 
-    Returns
-    -------
-    cwd : str
-        The path to the current working directory.
+    Returns:
+        str: The path to the current working directory.
     """
     return os.getcwd()
 
 
 def get_jackman_dir():
-    """
-    Retrieves the path to the installation folder of the Jackman module.
+    """Retrieves the path to the installation folder of the Jackman module.
 
-    Returns
-    -------
-    path : str
-        The path to the jackman module folder.
+    Returns:
+        str: The path to the jackman module folder.
     """
     return os.path.dirname(os.path.abspath(__file__))
 
 
 def set_dir(directory):
-    """
-    Changes the current directory to the specified directory.
+    """Changes the current directory to the specified directory.
 
-    Parameters
-    ----------
-    directory : str
-        The path to the directory to change to.
+    Args:
+        directory (str): The path to the directory to change the working directory to.
 
-    Returns
-    -------
-    complete : bool
-        Whether or not the directory was changed.
+    Returns:
+        bool: Whether or not changing the directory was successful.
     """
     try:
         os.chdir(str(directory))
@@ -55,13 +44,10 @@ def set_dir(directory):
 
 
 def cd_is_project():
-    """
-    Checks whether or not the current directory is a Jackman project.
+    """Checks whether or not the current directory is a Jackman project.
 
-    Returns
-    -------
-    project : bool
-        Whether or not the current directory can be marked as an initialized jackman project.
+    Returns:
+        bool: Whether or not the current directory is an initialized Jackman project.
     """
     if os.path.isfile('_jackman_config.yaml') or os.path.isfile('_jackman_config.yml'):
         return True
@@ -69,6 +55,11 @@ def cd_is_project():
 
 
 def setup_logging():
+    """Sets up default logging, so we can streamline it across multiple modules.
+
+    Returns:
+        None
+    """
     file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     stream_formatter = logging.Formatter('%(levelname)s - %(message)s')
 
@@ -84,18 +75,13 @@ def setup_logging():
 
 
 def load_yaml(file):
-    """
-    Loads a yaml-file into a dictionary.
+    """Loads the content of a yaml-file into a dictionary.
 
-    Parameters
-    ----------
-    file : str
-        The path to the file that should be loaded.
+    Args:
+        file (str): The path to the file that should be loaded.
 
-    Returns
-    -------
-    items : dict
-        A dict with all the items in the yaml-file.
+    Returns:
+        dict: A dict with all the items in the yaml-file.
     """
     if not is_yaml(file):
         log.error(f'Unable to read data from "{file}". It is not a yaml-file.')
@@ -110,41 +96,28 @@ def load_yaml(file):
 
 
 def is_yaml(file):
-    """
-    Checks whether or not the specified file is a yaml-file.
+    """Checks whether or not the specified file is a yaml-file.
 
-    Parameters
-    ----------
-    file : str
-        The relative path to the file that should be checked.
+    Args:
+        file (str): The relative path to the file that should be checked.
 
-    Returns
-    -------
-    bool
-        Whether or not the specified file is a yaml-file.
+    Returns:
+        bool: Whether or not the specified file is a yaml-file.
     """
     if file.endswith('.yaml') or file.endswith('.yml'):
         return True
     return False
 
 
-def minify_html(html):
-    # TODO: Make settings for minifying customizable
-    minified_html = htmlmin.minify(html,
-                                   remove_comments=True,
-                                   remove_empty_space=True,
-                                   remove_all_empty_space=False,
-                                   reduce_empty_attributes=True,
-                                   reduce_boolean_attributes=False,
-                                   remove_optional_attribute_quotes=True,
-                                   convert_charrefs=True,
-                                   keep_pre=False
-                                   )
-    return minified_html
-
-
 def load_files(path):
-    """Loads files into a list."""
+    """Indexes files in a path and loads them into a dict.
+
+    Args:
+        path (str): The path to the file that should be loaded into a dict.
+
+    Returns:
+          dict: All filenames in the specified path.
+    """
     files = {}
     for file in os.listdir(path):
         files[file] = os.path.relpath(file)
@@ -152,9 +125,39 @@ def load_files(path):
     return files
 
 
-class Expects(object):
+def get_config_value(keys):
+    """Retrieves a configuration value from the Jackman configuration file.
+
+    Args:
+        keys (str): The string of (nested) dictionary values.
+
+    Note:
+        You can find nested keys by introducing '.' in your ``keys`` value.
+        foo.bar will be looked up as: ``config[foo][bar]``
+
+    Returns:
+        dict/str: Dictionary with values if not fully nested, string with value if fully unnested.
     """
-    Context manager when we are expecting that an error could occur and we accept this.
+    config = {}
+    try:
+        config = load_yaml('_jackman_config.yaml')
+    except FileNotFoundError:
+        log.exception('Missing configuration file, are you sure _jackman_config.yaml exists?')
+    finally:
+        return reduce(lambda d, key: d.get(key, '') if isinstance(d, dict) else '', keys.split("."), config)
+
+
+class Expects(object):
+    """Context manager when we are expecting that an error could occur and we accept this.
+
+    Args:
+        expected_errors (list): A list of expected errors to skip.
+
+    Raises:
+        ValueError: The provided argument is not a list.
+
+    Attributes:
+        expected_errors (list): A list of expected errors to skip.
     """
     def __init__(self, expected_errors):
         if type(expected_errors) != list:
