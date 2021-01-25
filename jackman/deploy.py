10dec2ca86ffed0cecd9f11f677f60c5da5158ca
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-25 23:53:00 +0100

    Added deployment pipeline selector and documented entire deploy.py file

--- jackman/deploy.py
+++ jackman/deploy.py
@@ -1,4 +1,48 @@
-def main():
-    """Helps deploying your website via (S)FTP
+import logging
+from pkg_resources import iter_entry_points
+
+from jackman.helpers import get_config_value
+from jackman.errors import DeployUnknownPipelineError
+
+log = logging.getLogger(__name__)
+
+
+def main(selected_pipeline=get_config_value('deployment.default-pipeline')):
+    """Helps deploying your website via a chosen pipeline
+
+    Args:
+        selected_pipeline (str): the pipeline to use to deploy the website.
+
+    Returns:
+        None
+
+    Raises:
+        DeployUnknownPipelineError: the selected pipeline is not installed or is unknown.
     """
-    pass
+    configured_pipeline = None
+
+    for pipeline in iter_entry_points('jackman.deploy'):
+        if pipeline.name == selected_pipeline:
+            configured_pipeline = pipeline.load()
+
+    if configured_pipeline is None:
+        raise DeployUnknownPipelineError
+    else:
+        configured_pipeline()
+
+
+def get_pipelines():
+    """Retrieves all registered pipelines from installed modules
+
+    Returns:
+        dict: All registered pipelines.
+    """
+    pipelines = {}
+    for pipeline in iter_entry_points('jackman.deploy'):
+        if pipeline.name not in pipelines.keys():
+            pipelines[pipeline.name] = pipeline.load()
+            log.debug(f'Found and added {pipeline.name}')
+        else:
+            log.debug(f'{pipeline.name} was already loaded by another source.')
+
+    return pipelines
