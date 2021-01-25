55246c6748798299af7c6e04d67425883592cac5
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-26 00:55:03 +0100

    Fixed tests for help command to reflect changes throwing errors

--- tests/test_core_command_help.py
+++ tests/test_core_command_help.py
@@ -1,5 +1,8 @@
+import pytest
+
 from jackman.core import execute, show_help
 from jackman.build import main as build_main_function
+from jackman.errors import CoreHelpCommandTooLong, CoreUnknownCommandError
 
 
 def test_get_help_no_specified_command():
@@ -7,22 +10,16 @@ def test_get_help_no_specified_command():
     assert execute(argument_vector) == show_help()
 
 
-def test_get_help_specified_command_not_exists(caplog):
-    argument_vector = ['core.py', 'help', '']
-    execute(argument_vector)
-    assert caplog.records
-    for record in caplog.records:
-        assert record.levelname == "ERROR"
-        assert record.msg == f'The specified command {argument_vector[2]} is not recognized as a Jackman command.'
+def test_get_help_specified_command_not_exists():
+    argument_vector = ['core.py', 'help', 'weoifjewioufhewiufhewiufhri']
+    with pytest.raises(CoreUnknownCommandError):
+        execute(argument_vector)
 
 
-def test_get_help_specified_command_too_long(caplog):
+def test_get_help_specified_command_too_long():
     argument_vector = ['core.py', 'help', 'build', 'test']
-    execute(argument_vector)
-    assert caplog.records
-    for record in caplog.records:
-        assert record.levelname == "ERROR"
-        assert record.msg == 'The specified command is too long. Please enter only one command: jackman help <command>'
+    with pytest.raises(CoreHelpCommandTooLong):
+        execute(argument_vector)
 
 
 def test_get_help_command_exists(capsys):
