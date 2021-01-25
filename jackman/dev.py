5e09b48e9945d3c478bcb4fdfa9dbe853e27a0ae
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-26 00:41:23 +0100

    Updated docstrings to adhere to Google Docstring standards

--- jackman/dev.py
+++ jackman/dev.py
@@ -12,15 +12,24 @@ log = logging.getLogger(__name__)
 
 
 def main():
-    """Starts a local server that shows you your website in development
+    """Starts a local server that shows you your website in development.
     """
-    log.info('Starting a temporary build with mode "development"')
+    log.debug('Starting a temporary build with mode "development"')
     builder = Builder('development')
     builder.build()
     serve_forever(f'{get_cwd()}/{builder.tmp_dir}')
 
 
 def serve_forever(directory, port=8000):
+    """Serve a specified directory until interrupted.
+
+    Args:
+        directory (str): The path to the directory to serve on the local server.
+        port (int): The port that should be used to serve.
+
+    Raises:
+        FileNotFoundError: The specified directory to serve does not exist or is not accessible.
+    """
     log.debug(f'Moving into {directory}')
     if not set_dir(directory):
         raise FileNotFoundError(f'{directory} does not exist')
@@ -31,7 +40,7 @@ def serve_forever(directory, port=8000):
 
     with socketserver.TCPServer(("", port), tcp_handler) as dev_server:
         try:
-            log.info(f'Serving forever on localhost:{port}')
+            log.debug(f'Serving forever on localhost:{port}')
             webbrowser.open(f'http://localhost:{port}')
             dev_server.serve_forever()
         except KeyboardInterrupt:
@@ -39,7 +48,7 @@ def serve_forever(directory, port=8000):
             dev_server.shutdown()
             dev_server.server_close()
             dev_server.socket.close()
-            log.info('Successfully closed the server.')
+            log.debug('Successfully closed the server.')
             shutil.rmtree(directory)
             log.debug(f'Removed temporary directory {directory}')
         except Exception as e:
