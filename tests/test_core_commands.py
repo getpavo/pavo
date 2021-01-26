e2a0cec9323c88c26aee360f9211bff7d7bc4db4
j.veldhuis <jobbfveldhuis@gmail.com>
2021-01-26 01:21:11 +0100

    Added more tests for core commands module. [88% coverage]

--- tests/test_core_commands.py
+++ tests/test_core_commands.py
@@ -1,11 +1,18 @@
 import pytest
 
+from sys import argv
+
 from jackman.core import main, execute, show_help
-from jackman.errors import CoreUnknownCommandError
+from jackman.errors import CoreUnknownCommandError, CoreUnspecifiedCommandError
 
 
-def test_empty_call_to_main():
+def test_mock_empty_call_to_main():
     assert main(['filename']) == show_help()
+
+
+def test_actual_empty_call_to_main():
+    with pytest.raises(CoreUnknownCommandError):
+        assert main() == execute(argv)
 
 
 def test_execute_with_error_command():
@@ -14,29 +21,20 @@ def test_execute_with_error_command():
         execute(argument_vector)
 
 
-def test_execute_build():
-    pass
-
-
-def test_execute_dev():
-    pass
-
-
-def test_execute_command_outside_jackman_project():
-    pass
-
-
-def test_execute_create_inside_jackman_project():
-    pass
-
-
-def test_execute_create_outside_jackman_project():
-    pass
+def test_execute_with_too_few_arguments():
+    argument_vector = ['core.py']
+    with pytest.raises(CoreUnspecifiedCommandError):
+        execute(argument_vector)
 
 
 def test_execute_help():
     argument_vector = ['core.py', 'help']
     assert execute(argument_vector) == show_help()
+
+
+def test_execute_help_from_main():
+    argument_vector = ['core.py', 'help']
+    assert main(argument_vector) == show_help()
 
 
 def test_execute_deploy():
