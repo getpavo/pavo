aa8f8824811bc7fa4ca6d2c116f08528b17c48f0
j.veldhuis <jobbfveldhuis@gmail.com>
2021-02-15 22:07:58 +0100

    Working development server with live reloading

--- jackman/dev.py
+++ jackman/dev.py
@@ -1,12 +1,7 @@
 import shutil
 import logging
-
-import socketserver
-import http.server
-import webbrowser
-
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from httpwatcher import HttpWatcherServer
+from tornado.ioloop import IOLoop
 
 from jackman.build import Builder
 from jackman.helpers import get_cwd, set_dir, cd_is_project
@@ -23,84 +18,44 @@ def main():
 
 class DevelopmentServer:
     def __init__(self):
-        self.server = None
-        self.port = 8000
         self.builder = Builder('development')
-        self.directory = self.builder.tmp_dir
         self.project_directory = get_cwd() if cd_is_project() else None
-        self.observer = Observer()
-        self.handler = DevelopmentServerFileHandler(self.project_directory, self.build_temporary_directory)
+        self.directory = self.builder.tmp_dir
+        self.paths_to_watch = [
+            f'{self.project_directory}/_pages/',
+            f'{self.project_directory}/_posts/',
+            f'{self.project_directory}/_templates',
+        ]
+
+        self.server_settings = {
+            'ip': '127.0.0.1',
+            'port': 5556
+        }
+
+        self.server = HttpWatcherServer(
+            self.directory,
+            watch_paths=self.paths_to_watch,
+            on_reload=self.build_temporary_directory,
+            host=self.server_settings['ip'],
+            port=self.server_settings['port'],
+            watcher_interval=1.0,
+            recursive=True,
+            open_browser=True
+        )
 
     def run(self):
         self.build_temporary_directory()
-        self.observer.schedule(self.handler, self.project_directory, recursive=True)
-        self.observer.start()
+        self.server.listen()
         try:
-            self.serve_forever()
+            IOLoop.current().start()
         except KeyboardInterrupt:
-            self.shutdown()
-        self.observer.join()
-
-    def shutdown(self):
-        if self.server:
             self.server.shutdown()
-            self.server.server_close()
-            self.observer.stop()
-            log.debug('Successfully closed the server.')
-            shutil.rmtree(f'{self.project_directory}/{self.directory}')
-            log.debug(f'Removed temporary directory {self.directory}')
-
-    def serve_forever(self):
-        """Serve a specified directory until interrupted.
-
-        Raises:
-            FileNotFoundError: The specified directory to serve does not exist or is not accessible.
-        """
-        log.debug(f'Moving into {self.directory}')
-        if not set_dir(self.directory):
-            raise FileNotFoundError(f'{self.directory} does not exist')
-
-        tcp_handler = http.server.SimpleHTTPRequestHandler
-        socketserver.TCPServer.allow_reuse_address = True
-        log.debug(f'Created TCP Handler http.server.SimpleHTTPRequestHandler')
-
-        with socketserver.TCPServer(("", self.port), tcp_handler) as dev_server:
-            try:
-                self.server = dev_server
-                log.debug(f'Serving forever on localhost:{self.port}')
-                webbrowser.open(f'http://localhost:{self.port}')
-                self.server.serve_forever()
-            except KeyboardInterrupt:
-                self.shutdown()
-                dev_server.shutdown()
-            except Exception as e:
-                self.shutdown()
-                raise e from None
+            self.remove_leftovers()
 
     def build_temporary_directory(self):
         log.debug('Starting a temporary build with mode "development"')
         self.builder.build()
 
-
-class DevelopmentServerFileHandler(FileSystemEventHandler):
-    def __init__(self, directory, callback):
-        super().__init__()
-
-        # Paths that contain files that when modified should trigger a reload and rebuild
-        self.paths = [
-            f'{directory}/_pages/',
-            f'{directory}/_posts/',
-            f'{directory}/_templates',
-        ]
-
-        if hasattr(callback, '__call__'):
-            self.callback = callback
-        else:
-            raise TypeError('Callback for DevelopmentServerHandler should be a callable function.')
-
-    def on_any_event(self, event):
-        if not event.is_directory and not event.event_type == 'created':
-            for path in self.paths:
-                if event.src_path.startswith(path):
-                    print('Detected changes, reloading...')
-                    self.callback()
+    def remove_leftovers(self):
+        shutil.rmtree(self.directory)
+        log.debug(f'Removed temporary development server directory {self.directory}')
