1063477e4325dceb724a5bd7366045a0fa07cfa9
j.veldhuis <jobbfveldhuis@gmail.com>
2021-02-27 17:22:07 +0100

    Updated current test to reflect removal of show_help()

--- tests/test_core_commands.py
+++ tests/test_core_commands.py
@@ -1,18 +1,12 @@
 import pytest
 
-from sys import argv
-
-from jackman.core import main, execute, show_help
+from jackman.core import main, execute
 from jackman.errors import CoreUnknownCommandError, CoreUnspecifiedCommandError
 
 
 def test_mock_empty_call_to_main():
-    assert main(['filename']) == show_help()
-
-
-def test_actual_empty_call_to_main():
-    with pytest.raises(CoreUnknownCommandError):
-        assert main() == execute(argv)
+    with pytest.raises(CoreUnspecifiedCommandError):
+        main(['filename'])
 
 
 def test_execute_with_error_command():
@@ -25,16 +19,6 @@ def test_execute_with_too_few_arguments():
     argument_vector = ['core.py']
     with pytest.raises(CoreUnspecifiedCommandError):
         execute(argument_vector)
-
-
-def test_execute_help():
-    argument_vector = ['core.py', 'help']
-    assert execute(argument_vector) == show_help()
-
-
-def test_execute_help_from_main():
-    argument_vector = ['core.py', 'help']
-    assert main(argument_vector) == show_help()
 
 
 def test_execute_deploy():
