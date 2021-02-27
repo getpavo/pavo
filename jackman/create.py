55d6f8c8e022c1df644b05ad387e69810d26a800
j.veldhuis <jobbfveldhuis@gmail.com>
2021-02-28 00:56:11 +0100

    Changed logic in create so that a template can be pulled

--- jackman/create.py
+++ jackman/create.py
@@ -6,24 +6,29 @@ from distutils.dir_util import copy_tree
 from jackman.errors import CreateMissingProjectNameError, CreateNestedProjectError, CreateDirectoryExistsNotEmptyError
 from jackman.helpers import cd_is_project, get_cwd, Expects, get_jackman_dir
 
+# TODO: Do something with this logging
+# TODO: Find and destroy [#4]
+log = logging.getLogger(__name__)
 
-def main():
-    """Creates a new project folder in the current directory
+
+def main(name=None, get_hyde=True):
+    """Creates a new project folder in the current directory.
     """
-
-    # TODO: Add more logging to this
-    log = logging.getLogger(__name__)
+    if name is None:
+        raise CreateMissingProjectNameError
 
     if cd_is_project():
-        log.critical(f'Directory {get_cwd()} is already a project and Jackman projects should not be nested.')
-    else:
-        name = input('What is the name of your project? ')
+        raise CreateNestedProjectError
 
-        if os.path.exists(name) and os.path.isdir(name) and len(os.listdir(name)) != 0:
-            log.critical(f'The specified directory already exists and contains files.')
-            raise FileExistsError
+    if os.path.exists(name) and os.path.isdir(name) and len(os.listdir(name)) != 0:
+        raise CreateDirectoryExistsNotEmptyError
 
-        with Expects([FileExistsError]):
-            os.mkdir(name)
+    with Expects([FileExistsError]):
+        os.mkdir(name)
 
-        copy_tree(f'{get_jackman_dir()}/_templates/empty_project/', name)
+    copy_tree(f'{get_jackman_dir()}/_templates/empty_project/', name)
+
+    if get_hyde:
+        # TODO: Finish this so the Hyde theme is actually pulled
+        request = requests.get('https://api.github.com/repos/jackmanapp/hyde/releases/latest')
+        pass
