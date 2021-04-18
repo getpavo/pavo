b2a0ff4da82aeb3b745888a7707030f907edd4ec
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-18 19:28:17 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='jackman_core',
+                 version='0.1.0',
+                 description='Static Site Generation using Python made easy.',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 author='Job Veldhuis',
+                 author_email='job@baukefrederik.me',
+                 python_requires='>=3.6',
+                 url='https://github.com/jackmanapp/core',
+                 classifiers=[
+                     'Programming Language :: Python :: 3',
+                     'Development Status :: 2 - Pre-Alpha',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: OS Independent'
+                 ],
+                 install_requires=[
+                     'markdown2>=2.3.9',
+                     'htmlmin>=0.1.12',
+                     'httpwatcher>=0.5.2',
+                     'Jinja2>=2.11.3',
+                     'requests>=2.25.1',
+                     'tornado>=4.5.3',
+                     'libsass>=0.20.1',
+                     'python_frontmatter>=1.0.0',
+                     'PyYAML>=5.4.1'
+                 ],
+                 packages=setuptools.find_packages()
+                 )
