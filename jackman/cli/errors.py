2f710814435f42bb23b0c23b446f4307cf148a6e
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-21 15:39:41 +0200

    feat: Added default messages to exceptions, so their message is always helpful

--- jackman/cli/errors.py
+++ jackman/cli/errors.py
@@ -10,9 +10,12 @@ class UnspecifiedCommandError(CliError):
 
 class UnknownCommandError(CliError):
     """Raised when an unknown command is specified when executing jackman:core."""
-    pass
+    def __init__(self, message='The specified command could not be found, are you sure it is imported?'):
+        super().__init__(message)
 
 
 class InvalidExecutionDirectoryError(CliError):
     """Raised when calling any other function than help or create from outside a Jackman project directory."""
-    pass
+    def __init__(self, message='You are executing Jackman in an invalid Jackman project. Please create a new project '
+                               'or navigate to a valid project directory.'):
+        super().__init__(message)
