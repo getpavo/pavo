bdf70ffdde392842ad1511da621727bf8f2a0f19
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-23 01:45:10 +0200

    docs: Mark CLI module as internal API only.

---
+++ jackman/cli/__init__.py
@@ -0,0 +1,7 @@
+"""
+The Jackman Command Line Interface is an internal API only. It is not recommended to talk to the CLI directly.
+Instead hook onto functions from core and implement your own commands by creating a Python module that registers
+entry points for Jackman.
+
+You can find more information about creating plugins and using the public API in the Jackman documentation.
+"""
