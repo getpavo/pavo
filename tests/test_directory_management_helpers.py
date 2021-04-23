b3f4588802e876561d9795fbd1ba04c279b7d3f0
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-23 14:41:54 +0200

    test: Add tests for new directory helper functions.

--- tests/test_directory_management_helpers.py
+++ tests/test_directory_management_helpers.py
@@ -1,7 +1,7 @@
 import os
 import jackman.core
-
-from jackman.core.helpers import get_cwd, set_dir, cd_is_project, get_jackman_dir
+from jackman.core.helpers import get_cwd, set_dir, cd_is_project, get_jackman_dir, force_create_empty_directory, \
+    load_files
 
 
 def test_get_cwd():
@@ -15,6 +15,22 @@ def test_set_dir_not_exists(tmpdir):
 def test_set_dir_exists(tmpdir):
     tmpdir.mkdir('existing-folder')
     assert set_dir(f'{tmpdir}/existing-folder/') is True
+
+
+def test_force_create_empty_directory_dir_not_exists(tmpdir):
+    assert set_dir(f'{tmpdir}/new/') is False
+    force_create_empty_directory(f'{tmpdir}/new')
+    assert set_dir(f'{tmpdir}/new/') is True
+
+
+def test_force_create_empty_directory_dir_exists(tmpdir):
+    f = tmpdir.mkdir('new').join('.jackman')
+    f.write('empty')
+    assert set_dir(f'{tmpdir}/new/') is True
+    assert cd_is_project() is True
+    force_create_empty_directory(f'{tmpdir}/new')
+    assert set_dir(f'{tmpdir}/new/') is True
+    assert cd_is_project() is False
 
 
 def test_is_jackman_project_false(tmpdir):
@@ -31,3 +47,17 @@ def test_is_jackman_project_true(tmpdir):
 
 def test_get_jackman_directory():
     assert os.path.dirname(os.path.abspath(jackman.core.__file__)) == get_jackman_dir()
+
+
+def test_load_files_from_empty_directory(tmpdir):
+    tmpdir.mkdir('project')
+    assert load_files(f'{tmpdir}/project') == {}
+
+
+def test_load_files_from_directory(tmpdir):
+    f = tmpdir.mkdir('project').join('.jackman')
+    f.write('empty')
+    files = load_files(f'{tmpdir}/project')
+    assert len(files) == 1
+    assert '.jackman' in files
+    assert 'empty' not in files
