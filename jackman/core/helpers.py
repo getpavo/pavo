83c351bd922831046d8527ed1b9798ba8018a712
j.veldhuis <jobbfveldhuis@gmail.com>
2021-04-25 23:05:28 +0200

    feat: Add singleton decorator to helpers.

--- jackman/core/helpers.py
+++ jackman/core/helpers.py
@@ -132,3 +132,14 @@ class Expects(object):
             return True
         else:
             raise err
+
+
+def singleton(class_):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return get_instance
