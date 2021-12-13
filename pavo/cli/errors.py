ca709cf83d6f2ef6f1fc129f3e5a0e12ceb94ce1
j.veldhuis <jobbfveldhuis@gmail.com>
2021-12-13 14:23:51 +0100

    feat: Rework errors to default to using docstrings.

--- pavo/cli/errors.py
+++ pavo/cli/errors.py
@@ -1,21 +1,23 @@
-class CliError(Exception):
+from typing import Optional
+
+
+class PavoException(Exception):
+    """An exception that occurs on the """
+    def __init__(self, message: Optional[str] = None):
+        super().__init__(message or self.__doc__)
+
+
+class CliError(PavoException):
     """Parent class of all errors that occur when using CLI."""
-    pass
 
 
 class UnspecifiedCommandError(CliError):
-    """Raised when no command is specified when executing pavo:core."""
-    pass
+    """Please specify a command to run Pavo."""
 
 
 class UnknownCommandError(CliError):
-    """Raised when an unknown command is specified when executing pavo:core."""
-    def __init__(self, message: str = 'The specified command could not be found, are you sure it is imported?'):
-        super().__init__(message)
+    """The specified command could not be found, are you sure it is imported?"""
 
 
 class InvalidExecutionDirectoryError(CliError):
-    """Raised when calling any other function than help or create from outside a Pavo project directory."""
-    def __init__(self, message: str = 'You are executing Pavo in an invalid Pavo project. Please create a new project '
-                                      'or navigate to a valid project directory.'):
-        super().__init__(message)
+    """You are executing Pavo in an invalid Pavo project. Please create or navigate to a project."""
