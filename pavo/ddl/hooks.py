e671e05606171d360fc9b989e60220151e78c745
j.veldhuis <jobbfveldhuis@gmail.com>
2021-12-22 17:42:16 +0100

    feat: Update definition of invoker with properties and instantiation methods.

--- pavo/ddl/hooks.py
+++ pavo/ddl/hooks.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Callable
+from typing import Callable, Type, TypeVar
+
+# TypeVar for the generation methods in the Invoker class.
+_T = TypeVar('_T', bound='Invoker')
 
 
 class HookTypes(Enum):
@@ -14,11 +17,22 @@ class Invoker:
     module: str
     name: str
 
+    @classmethod
+    def from_callable(cls: Type[_T], func: Callable) -> _T:
+        return cls(
+            module=func.__module__,
+            name=func.__qualname__
+        )
+
+    @property
+    def unique_name(self):
+        return f'{self.module}.{self.name}'
+
 
 @dataclass
 class Hook:
     func: Callable
-    type_: HookTypes
+    type: HookTypes
     invoker: Invoker
 
     def __call__(self, *args, **kwargs):
