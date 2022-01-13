0153b07f691e01ad84f02b010057f5b5f62f919a
j.veldhuis <jobbfveldhuis@gmail.com>
2022-01-13 17:32:29 +0100

    docs: Remove outdated docstring.

--- pavo/cli/__init__.py
+++ pavo/cli/__init__.py
@@ -3,10 +3,6 @@ The Pavo Command Line Interface is an internal API only. It is not recommended t
 Instead, hook onto functions from core and implement your own commands by creating a Python module that registers
 entry points for Pavo.
 
-If you wish to communicate using the CLI internal message API, you can hook your function into the broadcast.
-Broadcast is a singleton class that should not be instantiated. Instead, you can send a message to the service.
-This means that messages sent by Broadcast are not live, instead fetched before and after a function call.
-
 You can find more information about creating plugins and using the public API in the Pavo documentation.
 """
 import pavo.cli._errors as errors
