b20be2303c0f8e791135dedbcc27a8a53c21951d
j.veldhuis <jobbfveldhuis@gmail.com>
2022-02-09 14:34:28 +0100

    chore(workflow): Remove deprecated scripts directory in favor of Makefile.

--- scripts/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! command -v poetry &> /dev/null
-then
-    echo "Poetry could not be found, cannot execute in virtual environment."
-    exit 1
-fi
-
-poetry run python3 -m pylint ./pavo
