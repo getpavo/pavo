bda774480a1b0cf945bd08ad21330da3d9b9c323
Job Veldhuis <jobbfveldhuis@gmail.com>
2022-05-04 00:02:52 +0200

    docs: Update docstring for commands module.

--- pavo/commands/__init__.py
+++ pavo/commands/__init__.py
@@ -1,5 +1,5 @@
 """
-The commands module contains all default commands for Pavo. The builtins are a full internal API.
+The commands module contains all default commands for Pavo. The commands are a full internal API.
 Please refer to the documentation for plugins and scripts to alter / extend Pavo builtin commands.
 """
 from ._build import Build
