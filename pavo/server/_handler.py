1552d1ad39b0cd7e37ad45d2a53fb720346e9f15
Job Veldhuis <jobbfveldhuis@gmail.com>
2022-06-06 00:01:15 +0200

    revert: Remove injection of websocket.

--- pavo/server/_handler.py
+++ pavo/server/_handler.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Optional, Awaitable, Generator
 
 import tornado.web
@@ -5,23 +6,10 @@ import tornado.web
 
 class StaticFileHandler(tornado.web.StaticFileHandler):
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
-        pass
+        raise NotImplementedError("Intentionally not implemented")
 
     def set_extra_headers(self, path: str) -> None:
+        # Disallow any form of caching
         self.set_header(
             "Cache-Control", "no-store, no-cache, must-revalidate, max-age=0"
         )
-
-    def get_content(
-        self, abspath: str, start: Optional[int] = None, end: Optional[int] = None
-    ) -> Generator[bytes, None, None]:
-        websocket_script_injection = """<script type="application/javascript" src="/websocket.js"></script>
-        </body>
-        """.encode(
-            "utf-8"
-        )
-
-        # TODO: Fix content-length error when retrieving file
-        if self.get_content_type() == "text/html":
-            with open(abspath, "rb") as file:
-                yield file.read().replace(b"</body>", websocket_script_injection)
