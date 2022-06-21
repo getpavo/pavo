2a17b86ccb99e2b7b54c6718825b190ac1e296e6
Job Veldhuis <jobbfveldhuis@gmail.com>
2022-06-21 16:18:47 +0200

    feat: Export version globals from `app` module.

--- pavo/app/__init__.py
+++ pavo/app/__init__.py
@@ -1,8 +1,1 @@
-"""
-The Pavo Command Line Interface is an internal API only. It is not recommended talking to the CLI directly.
-Instead, hook onto functions from core and implement your own commands by creating a Python module that registers
-entry points for Pavo.
-
-You can find more information about creating plugins and using the public API in the Pavo documentation.
-"""
-from pavo.app._cli import PavoApp
+from ._version import DISTRIBUTION_VERSION, CONFIGURATION_VERSION
